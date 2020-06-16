875a9a0c135d1d2c17856614f133396ed97583f5
NFedin <niks1995.fedin@gmail.com>
2020-06-16 19:40:05 +0300

    add packages and ArrayStorage.update

--- src/MainArray.java
+++ src/MainArray.java
@@ -1,9 +1,12 @@
+import com.urise.webapp.model.Resume;
+import com.urise.webapp.storage.ArrayStorage;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 /**
- * Interactive test for ArrayStorage implementation
+ * Interactive test for com.urise.webapp.storage.ArrayStorage implementation
  * (just run, no need to understand)
  */
 public class MainArray {
@@ -11,7 +14,7 @@ public class MainArray {
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        Resume r;
+        com.urise.webapp.model.Resume r;
         while (true) {
             System.out.print("Введите одну из команд - (list | save uuid | delete uuid | get uuid | clear | exit): ");
             String[] params = reader.readLine().trim().toLowerCase().split(" ");
@@ -32,7 +35,7 @@ public class MainArray {
                     break;
                 case "save":
                     r = new Resume();
-                    r.uuid = uuid;
+                    r.setUuid(uuid);
                     ARRAY_STORAGE.save(r);
                     printAll();
                     break;
--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -1,3 +1,7 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.model.Resume;
+
 import static java.lang.System.arraycopy;
 import static java.util.Arrays.copyOfRange;
 
@@ -8,51 +12,65 @@ public class ArrayStorage {
     private Resume[] storage = new Resume[10000];
     private int length = 0;
 
-    void clear() {
+    public void clear() {
         for (int i = 0; i < length; i++) {
             storage[i] = null;
         }
         length = 0;
     }
 
-    void save(Resume r) {
-        if (findResumeIndex(r.uuid) == -1) {
-            storage[length] = r;
-            length++;
+    public void update(Resume r) {
+        int index = findResumeIndex(r.getUuid());
+        if (index != -1) {
+            storage[index] = r;
+        } else {
+            System.out.println("ERROR: This resume not exist");
         }
     }
 
-    Resume get(String uuid) {
+    public void save(Resume r) {
+        if (findResumeIndex(r.getUuid()) == -1) {
+            storage[length] = r;
+            length++;
+        } else {
+            System.out.println("ERROR: This resume exist");
+        }
+    }
+
+    public Resume get(String uuid) {
         int index = findResumeIndex(uuid);
         if (index != -1) {
             return storage[index];
         } else {
+            System.out.println("ERROR: This resume not exist");
             return null;
         }
     }
 
-    void delete(String uuid) {
+    public void delete(String uuid) {
         int index = findResumeIndex(uuid);
         if (index != -1) {
             length--;
             arraycopy(storage, index + 1, storage, index, length - index);
+        } else {
+            System.out.println("ERROR: This resume not exist");
         }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
+    public Resume[] getAll() {
         return copyOfRange(storage, 0, length);
     }
 
-    int size() {
+    public int size() {
         return length;
     }
 
     private int findResumeIndex(String uuid) {
         for (int i = 0; i < length; i++) {
-            if (uuid.equals(storage[i].uuid)) {
+            if (uuid.equals(storage[i].getUuid())) {
                 return i;
             }
         }
